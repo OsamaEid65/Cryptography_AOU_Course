fdb889139f6ffe893f231fca2de7e4e02cd26eba
osama <osama@gmail.com>
2024-01-27 17:07:31 +0200

    ubdate number 1

--- Battery_Cipher/Battery_Cipher.py
+++ Battery_Cipher/Battery_Cipher.py
@@ -6,11 +6,11 @@ Key Range : 1 - 24
 date : 25/1/2024
 """
 import math
-plain_text = input("Enter the plain text : ")
+plain_text = input("Enter the plain text : ").lower()
 key = eval(input("Enter the key : "))
 alphapt="abcdefghigklmnopqrstuvwxyz"
-
-def battery_cipher_encrypt_half(plain_text,key):
+mode_of_operation = input("Enter the mode of operation : ").lower()
+def battery_cipher_encrypt_half_even_char(plain_text,key):
     if len(plain_text)%2 == 0:
         list1=list(plain_text)
 
@@ -29,7 +29,7 @@ def battery_cipher_encrypt_half(plain_text,key):
     else:
         print("Error : The length of the plain text must be even")
         exit()
-def battery_cipher_decrypt_half(cipher_text,key):
+def battery_cipher_decrypt_half_even_char(cipher_text,key):
     if len(cipher_text)%2 == 0:
         list1=list(cipher_text)
         list2 = list1[0:len(list1) // 2]
@@ -43,7 +43,7 @@ def battery_cipher_decrypt_half(cipher_text,key):
 
         plain_text = ''.join(list2 + list3)
         return plain_text
-def battery_cipher_encrypt_even(plain_text,key):
+def battery_cipher_encrypt_half_odd_char(plain_text,key):
     if len(plain_text)%2 != 0:
         list1=list(plain_text)
         list2=list1[0:math.ceil(len(list1)/2)]
@@ -57,7 +57,7 @@ def battery_cipher_encrypt_even(plain_text,key):
     else:
         print("Error : The length of the plain text must be odd")
         exit()
-def battery_cipher_decrypt_even(cipher_text,key):
+def battery_cipher_decrypt_half_odd_char(cipher_text,key):
     if len(cipher_text)%2 != 0:
         list1=list(cipher_text)
         list2 = list1[0:math.ceil(len(list1)/2)]
@@ -68,15 +68,93 @@ def battery_cipher_decrypt_even(cipher_text,key):
             list3[i] = alphapt[(alphapt.index(list3[i]) + key) % 26]
         plain_text = ''.join(list2 + list3)
         return plain_text
+def battery_cipher_encrypt_even_for_even_char(plain_text,key):
+    transformed_text = ""
+    if len(plain_text)%2 == 0:
+        list1=list(plain_text)
+        for i in range(0,len(list1),4):
+            list2 = list1[i:i+2]
+            list3= list1[i+2:i+4]
+            process_on_list2=""
+            process_on_list3=""
+            for char in list2:
+                process_on_list2+=alphapt[(alphapt.index(char)+key)%26]
+            for char in list3:
+                process_on_list3+=alphapt[(alphapt.index(char)-key)%26]
+            transformed_text+=process_on_list2+process_on_list3
+    return transformed_text
+def battery_cipher_decrypt_even_for_even_cahr(plain_text,key):
+    transformed_text = ""
+    if len(plain_text)%2 == 0:
+        list1=list(plain_text)
+        for i in range(0,len(list1),4):
+            list2 = list1[i:i+2]
+            list3= list1[i+2:i+4]
+            process_on_list2=""
+            process_on_list3=""
+            for char in list2:
+                process_on_list2+=alphapt[(alphapt.index(char)-key)%26]
+            for char in list3:
+                process_on_list3+=alphapt[(alphapt.index(char)+key)%26]
+            transformed_text+=process_on_list2+process_on_list3
+    return transformed_text
 
+def Battery_cipher_encrypt_even_for_odd_char(plain_text,key):
+    transformed_text = ""
+    if len(plain_text)%2 != 0:
+        list1=list(plain_text)
+        for i in range(0,len(list1),4):
+            list2 = list1[i:i+2]
+            list3= list1[i+2:i+4]
+            process_on_list2=""
+            process_on_list3=""
+            for char in list2:
+                process_on_list2+=alphapt[(alphapt.index(char)+key)%26]
+            for char in list3:
+                process_on_list3+=alphapt[(alphapt.index(char)-key)%26]
+            transformed_text+=process_on_list2+process_on_list3
+    return transformed_text
+def battery_cipher_decrypt_even_for_odd_char(plain_text,key):
+    transformed_text = ""
+    if len(plain_text)%2 != 0:
+        list1=list(plain_text)
+        for i in range(0,len(list1),4):
+            list2 = list1[i:i+2]
+            list3= list1[i+2:i+4]
+            process_on_list2=""
+            process_on_list3=""
+            for char in list2:
+                process_on_list2+=alphapt[(alphapt.index(char)-key)%26]
+            for char in list3:
+                process_on_list3+=alphapt[(alphapt.index(char)+key)%26]
+            transformed_text+=process_on_list2+process_on_list3
+    return transformed_text
 
-if len(plain_text)%2 ==0:
-    Cipher_Text=battery_cipher_encrypt_half(plain_text,key)
-    print(f"Cipher Text : {Cipher_Text}")
-    plain_text=battery_cipher_decrypt_half(Cipher_Text,key)
-    print(f"Plain Text : {plain_text}")
+if key > 24 or key < 1:
+    print("Error : The key must be between 1 and 24")
+    exit()
+if mode_of_operation == "half":
+    if len(plain_text)%2 ==0:
+        Cipher_Text=battery_cipher_encrypt_half_even_char(plain_text,key)
+        print(f"Cipher Text : {Cipher_Text}")
+        plain_text=battery_cipher_decrypt_half_even_char(Cipher_Text,key)
+        print(f"Plain Text : {plain_text}")
+    else:
+        Cipher_Text = battery_cipher_encrypt_half_odd_char(plain_text, key)
+        print(f"Cipher Text : {Cipher_Text}")
+        plain_text = battery_cipher_decrypt_half_odd_char(Cipher_Text, key)
+        print(f"Plain Text : {plain_text}")
+elif mode_of_operation == "even":
+    if len(plain_text)%2 ==0:
+        Cipher_Text=battery_cipher_encrypt_even_for_even_char(plain_text,key)
+        print(f"Cipher Text : {Cipher_Text}")
+        plain_text=battery_cipher_decrypt_even_for_even_cahr(Cipher_Text,key)
+        print(f"Plain Text : {plain_text}")
+    else:
+        Cipher_Text = Battery_cipher_encrypt_even_for_odd_char(plain_text, key)
+        print(f"Cipher Text : {Cipher_Text}")
+        plain_text = battery_cipher_decrypt_even_for_odd_char(Cipher_Text, key)
+        print(f"Plain Text : {plain_text}")
 else:
-    Cipher_Text = battery_cipher_encrypt_even(plain_text, key)
-    print(f"Cipher Text : {Cipher_Text}")
-    plain_text = battery_cipher_decrypt_even(Cipher_Text, key)
-    print(f"Plain Text : {plain_text}")
+    print("Error : The mode of operation must be half or even")
+    exit()
